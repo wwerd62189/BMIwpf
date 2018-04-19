54202477a1b798d86eeb98615540a7d8b98d732d
m305-64\w401 <tommy555553@gmail.com>
2018-04-19 17:34:17 +0800

    BMI計算

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入身高:");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.Write("請輸入體重:");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            double BMI = B/(A*A/10000);
+            Console.WriteLine("BMI=" + BMI);
+
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
